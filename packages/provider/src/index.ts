d962ff92fb98a59bc3568068a3cedb827bdf66e1
RealShadowNova <real.shadow.nova@gmail.com>
2022-05-11 12:38:23 -0400

    feat(provider): port tests package

--- packages/provider/src/index.ts
+++ packages/provider/src/index.ts
@@ -2,5 +2,6 @@ export * from './lib/functions';
 export * from './lib/structures/JoshProvider';
 export * from './lib/structures/JoshProviderError';
 export * from './lib/types';
+export * from './tests/runProviderTest';
 
 export const version = '[VI]{version}[/VI]';
