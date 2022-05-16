a99f37f82c59b13aad22d91e11edc7d8cbad1578
RealShadowNova <real.shadow.nova@gmail.com>
2022-05-16 17:57:33 -0400

    chore(middleware): refactor workspace

--- packages/middleware/src/index.ts
+++ packages/middleware/src/index.ts
@@ -3,5 +3,3 @@ import 'reflect-metadata';
 export * from './lib/decorators';
 export * from './lib/Middleware';
 export * from './lib/MiddlewareStore';
-
-export const version = '[VI]{version}[/VI]';
