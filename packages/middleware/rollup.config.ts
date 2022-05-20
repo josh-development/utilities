736f7925e87c45c389e5b9bd9178f5a84ca4e76b
RealShadowNova <real.shadow.nova@gmail.com>
2022-05-20 16:24:59 -0400

    chore(build): revert to rollup

---
+++ packages/middleware/rollup.config.ts
@@ -0,0 +1,35 @@
+import { resolve } from 'node:path';
+import { defineConfig } from 'rollup';
+import cleaner from 'rollup-plugin-cleaner';
+import typescript from 'rollup-plugin-typescript2';
+
+export default defineConfig({
+  input: './src/index.ts',
+  output: [
+    {
+      file: './dist/index.js',
+      format: 'cjs',
+      exports: 'named',
+      sourcemap: true
+    },
+    {
+      file: './dist/index.mjs',
+      format: 'es',
+      exports: 'named',
+      sourcemap: true
+    },
+    {
+      file: './dist/index.umd.js',
+      format: 'umd',
+      name: 'JoshMiddleware',
+      sourcemap: true,
+      globals: {
+        '@joshdb/provider': 'Joshprovider',
+        '@sapphire/utilities': 'SapphireUtilities',
+        'reflect-metadata': 'ReflectMetadata'
+      }
+    }
+  ],
+  external: ['@joshdb/provider', '@sapphire/utilities', 'reflect-metadata'],
+  plugins: [cleaner({ targets: ['./dist'] }), typescript({ tsconfig: resolve(process.cwd(), 'src', 'tsconfig.json') })]
+});
