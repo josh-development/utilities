c17b65f03442f8eb98c89ac5d407606264138530
Hezekiah Hendry <real.shadow.nova@gmail.com>
2022-05-20 15:14:24 -0400

    chore(build): revert to rollup (#17)

---
+++ packages/provider/rollup.config.ts
@@ -0,0 +1,33 @@
+import { resolve } from 'node:path';
+import { defineConfig } from 'rollup';
+import cleaner from 'rollup-plugin-cleaner';
+import typescript from 'rollup-plugin-typescript2';
+
+export default defineConfig({
+  input: './src/index.ts',
+  output: [
+    {
+      file: './dist/index.js',
+      format: 'cjs',
+      exports: 'named',
+      sourcemap: true
+    },
+    {
+      file: './dist/index.mjs',
+      format: 'es',
+      exports: 'named',
+      sourcemap: true
+    },
+    {
+      file: './dist/index.umd.js',
+      format: 'umd',
+      name: 'JoshProvider',
+      sourcemap: true,
+      globals: {
+        '@sapphire/utilities': 'SapphireUtilities'
+      }
+    }
+  ],
+  external: ['@sapphire/utilities'],
+  plugins: [cleaner({ targets: ['./dist'] }), typescript({ tsconfig: resolve(process.cwd(), 'src', 'tsconfig.json') })]
+});
---
+++ packages/serialize/rollup.config.ts
@@ -0,0 +1,31 @@
+import { resolve } from 'node:path';
+import { defineConfig } from 'rollup';
+import cleaner from 'rollup-plugin-cleaner';
+import typescript from 'rollup-plugin-typescript2';
+
+export default defineConfig({
+  input: './src/index.ts',
+  output: [
+    {
+      file: './dist/index.js',
+      format: 'cjs',
+      exports: 'named',
+      sourcemap: true
+    },
+    {
+      file: './dist/index.mjs',
+      format: 'es',
+      exports: 'named',
+      sourcemap: true
+    },
+    {
+      file: './dist/index.umd.js',
+      format: 'umd',
+      name: 'JoshSerialize',
+      sourcemap: true,
+      globals: {}
+    }
+  ],
+  external: [],
+  plugins: [cleaner({ targets: ['./dist'] }), typescript({ tsconfig: resolve(process.cwd(), 'src', 'tsconfig.json') })]
+});
