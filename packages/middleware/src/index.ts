01317c2763264045e0b0dc5aee2dd322d789c814
Hezekiah Hendry <real.shadow.nova@gmail.com>
2022-05-20 16:50:04 -0400

    feat(middleware): add middleware package (#9)
    * feat(middleware): add middleware package
    
    * chore(scripts): fix build deps
    
    * chore(deps): regen lockfile
    
    * feat(middleware): add decorators
    
    * chore(deps): regen lockfile
    
    * chore(middleware): refactor workspace

---
+++ packages/middleware/src/index.ts
@@ -0,0 +1,5 @@
+import 'reflect-metadata';
+
+export * from './lib/decorators';
+export * from './lib/Middleware';
+export * from './lib/MiddlewareStore';
---
+++ packages/middleware/src/lib/MiddlewareStore.ts
@@ -0,0 +1,74 @@
+import { JoshProvider, Method, Trigger } from '@joshdb/provider';
+import type { NonNullObject } from '@sapphire/utilities';
+import type { Middleware } from './Middleware';
+
+/**
+ * The store to contain {@link Middleware} classes.
+ * @since 2.0.0
+ */
+export class MiddlewareStore<StoredValue = unknown> extends Map<string, Middleware<NonNullObject, StoredValue>> {
+  /**
+   * The link {@link JoshProvider} instance for this store.
+   * @since 2.0.0
+   */
+  public provider: JoshProvider<StoredValue>;
+
+  public constructor(options: MiddlewareStoreOptions<StoredValue>) {
+    super();
+
+    const { provider } = options;
+
+    this.provider = provider;
+  }
+
+  /**
+   * Gets an array of middlewares.
+   * @since 2.0.0
+   * @returns The array of middlewares.
+   */
+  public array(): Middleware<NonNullObject, StoredValue>[] {
+    return Array.from(this.values());
+  }
+
+  /**
+   * Get pre provider middlewares by method.
+   * @since 2.0.0
+   * @param method The method to filter by.
+   * @returns The middlewares after filtered.
+   */
+  public getPreMiddlewares(method: Method): Middleware<NonNullObject, StoredValue>[] {
+    return this.filterByCondition(method, Trigger.PreProvider);
+  }
+
+  /**
+   * Get post provider middlewares by method.
+   * @since 2.0.0
+   * @param method The method to filter by.
+   * @returns The middlewares after filtered.
+   */
+  public getPostMiddlewares(method: Method): Middleware<NonNullObject, StoredValue>[] {
+    return this.filterByCondition(method, Trigger.PostProvider);
+  }
+
+  /**
+   * Filter middlewares by their conditions.
+   * @since 2.0.0
+   * @param method
+   * @param trigger
+   * @returns
+   */
+  private filterByCondition(method: Method, trigger: Trigger): Middleware<NonNullObject, StoredValue>[] {
+    const middlewares = this.array().filter((middleware) =>
+      trigger === Trigger.PreProvider ? middleware.conditions.pre.includes(method) : middleware.conditions.post.includes(method)
+    );
+
+    const withPositions = middlewares.filter((middleware) => middleware.position !== undefined);
+    const withoutPositions = middlewares.filter((middleware) => middleware.position !== undefined);
+
+    return [...withPositions.sort((a, b) => a.position! - b.position!), ...withoutPositions];
+  }
+}
+
+export interface MiddlewareStoreOptions<StoredValue = unknown> {
+  provider: JoshProvider<StoredValue>;
+}
