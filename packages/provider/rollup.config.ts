a88fe3707cf607b37398a5c9c135a12e1cb73241
Hezekiah Hendry <hezekiah.hendry@gmail.com>
2022-09-17 16:57:38 -0400

    feat!: remove `umd` bundle (#92)

--- packages/provider/rollup.config.ts
+++ packages/provider/rollup.config.ts
@@ -17,16 +17,6 @@ export default defineConfig({
       format: 'es',
       exports: 'named',
       sourcemap: true
-    },
-    {
-      file: './dist/index.umd.js',
-      format: 'umd',
-      name: 'JoshProvider',
-      sourcemap: true,
-      globals: {
-        '@sapphire/utilities': 'SapphireUtilities',
-        vitest: 'Vitest'
-      }
     }
   ],
   external: ['@sapphire/utilities', 'reflect-metadata', 'vitest'],
