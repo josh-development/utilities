c4c80c104c3e59c87d2cf52e2bb1a49fcbbd9d77
DanCodes <dan@dancodes.online>
2022-10-29 14:27:40 +0100

    fix(query): change interface

--- packages/provider/src/lib/types/Query.ts
+++ packages/provider/src/lib/types/Query.ts
@@ -1,15 +1,21 @@
 export interface QueryOptions<Value> {
-  ne: Value;
-  gt: Value;
-  gte: Value;
-  lt: Value;
-  lte: Value;
-  in: Value[];
-  nin: Value[];
-  regex: RegExp;
+  ne?: Value;
+  gt?: Value;
+  gte?: Value;
+  lt?: Value;
+  lte?: Value;
+  in?: Value[];
+  nin?: Value[];
+  regex?: RegExp;
 }
 
+export interface QueryOptionsValue<Value> extends QueryOptions<Value> {
+  path?: [];
+}
+
+// TODO: Need Nova's ts expertise to match type of Value with the path specified
+
 export interface Query<StoredValue> {
-  key: string | QueryOptions<string>;
-  value: StoredValue | QueryOptions<StoredValue>;
+  key?: string | QueryOptions<string>;
+  value?: StoredValue | QueryOptionsValue<StoredValue>;
 }
