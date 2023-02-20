7e35cf736c13188087422c11b1396d4a1a153915
DanCodes <dan@dancodes.online>
2023-02-20 22:46:00 +0000

    ci: use .github actions (#167)
    * chore(runProviderTest): remove from main exports
    
    * chore(rollup): output extra test file
    
    * chore(tests): possible fix
    
    * ci: use github repo action
    
    * ci: fix cov report

--- packages/provider/rollup.config.ts
+++ packages/provider/rollup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'rollup';
 import cleaner from 'rollup-plugin-cleaner';
 import typescript from 'rollup-plugin-typescript2';
 
-export default defineConfig({
+const main = defineConfig({
   input: './src/index.ts',
   output: [
     {
@@ -19,6 +19,30 @@ export default defineConfig({
       sourcemap: true
     }
   ],
-  external: ['@sapphire/utilities', 'reflect-metadata', 'vitest'],
+  external: ['@sapphire/utilities', 'reflect-metadata'],
   plugins: [cleaner({ targets: ['./dist'] }), typescript({ tsconfig: resolve(process.cwd(), 'src', 'tsconfig.json') })]
 });
+
+const tests = defineConfig({
+  input: './src/tests/index.ts',
+  output: [
+    {
+      file: './dist/tests.js',
+      format: 'cjs',
+      exports: 'named',
+      sourcemap: true
+    },
+    {
+      file: './dist/tests.mjs',
+      format: 'es',
+      exports: 'named',
+      sourcemap: true
+    }
+  ],
+  external: ['@sapphire/utilities', 'reflect-metadata', 'vitest'],
+  plugins: [typescript({ tsconfig: resolve(process.cwd(), 'src', 'tsconfig.json') })]
+});
+
+const config = [main, tests];
+
+export default defineConfig(config);
---
+++ packages/provider/src/tests/index.ts
@@ -0,0 +1,1 @@
+export * from './runProviderTest';
